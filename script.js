20f1ea49f2351b2991d0da2460b8d1077571cd8e
ავთანდილ საგინაშვილი <120257059+AvtandilSaginashvili@users.noreply.github.com>
2023-05-05 20:18:14 +0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+function gamotvla() {
+    var x = document.getElementById ("inp1").value;
+    var y = document.getElementById ("inp2").value;
+    var mimateba = document.getElementById ("mimateba");
+    var gamokleba = document.getElementById ("gamokleba");
+    var gamravleba = document.getElementById ("gamravleba");
+    var gayopa = document.getElementById ("gayopa");
+
+    if (mimateba.checked) {
+        document.getElementById ("pasuxi").innerHTML = Number(x) + Number(y);
+    }else if (gamokleba.checked) {
+        document.getElementById ("pasuxi").innerHTML = Number(x) - Number(y);
+    }else if (gamravleba.checked) {
+        document.getElementById ("pasuxi").innerHTML = Number(x) * Number(y);
+    }else if (gayopa.checked) {
+        document.getElementById ("pasuxi").innerHTML = Number(x) / Number(y);
+    }
+}
